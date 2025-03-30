6b2005cc4a9c270c9b1e9b79c6e7dc09b4b96f40
constructorable <acker_oliver@yahoo.de>
2025-03-30 23:24:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -624,7 +624,7 @@ function initSignatureCanvas(canvasId) {
     function saveSignatureToLocalStorage(canvas, canvasId) {
         const signatureData = canvas.toDataURL();
         
-        const expirationTime = new Date().getTime() + 2700 * 1000; // 45 Minuten (in Millisekunden) werden die Unterschriften gespeichert. 
+        const expirationTime = new Date().getTime() + 30 * 1000; // 45 Minuten (in Millisekunden) werden die Unterschriften gespeichert. 
         const signatureObject = {
             data: signatureData,
             expiration: expirationTime
