891fc8e923124f69e3ac8efb02ffe5ffded10efb
constructorable <acker_oliver@yahoo.de>
2025-05-01 10:08:26 +0200

    Update mailcheck.js

--- mailcheck.js
+++ mailcheck.js
@@ -1,6 +1,6 @@
-// Copyright - Oliver Acker, acker_oliver@yahoo.de
-// mailcheck.js
-// Version 3.25_beta
+/* Copyright - Oliver Acker, acker_oliver@yahoo.de
+mailcheck.js
+Version 3.34_beta */
 
 function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
